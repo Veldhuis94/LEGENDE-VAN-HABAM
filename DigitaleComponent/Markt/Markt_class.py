1811203a5716d3ebfe2e09c27a4bfd66cc46473b
mhmodo10 <45037687+mhmodo10@users.noreply.github.com>
2019-01-04 20:44:42 +0100

    Update Markt_class.py

--- DigitaleComponent/Markt/Markt_class.py
+++ DigitaleComponent/Markt/Markt_class.py
@@ -3,53 +3,71 @@ from Utilities.Button import Button
 import random
 class Markt:
     def __init__(self):
-    self.tellers=tellers()
-    self.showcard=False
-    self.grain=tellers.grain
-    self.metal=tellers.metal
-    self.stone=tellers.brick
-    self.wood=tellers.wood
-    self.capacity=tellers.capacity
-    self.boughtbags=0
-    self.bagcount=tellers.bagcount
-    self.enoughbrick=True
-    self.enoughgrain=True
-    self.price=3
-    self.effect10=tellers.effect10
-    self.effect14=False
-    self.clicked=False
-    self.resourcename='nothing'
-    self.enoughmats=True
-    self.chosenmat=5
-    self.pressed=False
-    self.power1=tellers.power1
-    self.power2=tellers.power2
-    self.power3=tellers.power3
-    self.power4=tellers.power4
-    self.enoughmatspp=True
-    def powerup1(self):
-        global power1
-        global wood
-        global stone
-        global metal
-        global grain
-        global enoughmatspp
+        self.tellers=tellers()
+        self.showcard=False
+        self.grain=self.tellers.grain
+        self.metal=self.tellers.metal
+        self.stone=self.tellers.brick
+        self.wood=self.tellers.wood
+        self.capacity=self.tellers.capacity
+        self.boughtbags=0
+        self.bagcount=self.tellers.bagcount
+        self.enoughbrick=True
+        self.enoughgrain=True
+        self.price=3
+        self.effect10=self.tellers.effect10
+        self.effect14=False
+        self.clicked=False
+        self.resourcename='nothing'
+        self.enoughmats=True
+        self.chosenmat=5
+        self.pressed=False
+        self.power1=self.tellers.power1
+        self.power2=self.tellers.power2
+        self.power3=self.tellers.power3
+        self.power4=self.tellers.power4
+        self.enoughmatspp=True
+        self.buttons=[Button(200, 150, txt="EffectKaart", bgColor=(255, 255, 255), onClick =self.chosencard, w = 250, h = 50),
+                Button(200, 250, txt="Reset", bgColor=(255, 255, 255), onClick =self.reset, w = 250, h = 50),
+                Button(1300, 250, txt="Rugzak", bgColor=(255, 255, 255), onClick =self.backpack, w = 250, h = 50),
+                Button(850, 470, txt="Gebruik", bgColor=(255, 255, 255), onClick =self.Effect14Used, w = 250, h = 50),
+                Button(1300, 550, txt="Krachtpunt P1", bgColor=(255, 255, 255), onClick =self.powerup1, w = 250, h = 50),
+                Button(1300, 650, txt="Krachtpunt P2", bgColor=(255, 255, 255), onClick =self.powerup2, w = 250, h = 50),
+                Button(1300, 750, txt="Krachtpunt P3", bgColor=(255, 255, 255), onClick =self.powerup3, w = 250, h = 50),
+                Button(1300, 850, txt="Krachtpunt P4", bgColor=(255, 255, 255), onClick =self.powerup4, w = 250, h = 50),]
+    
+    
+        self.resourcesbuttons=[Button(200,600, txt="Hout", bgColor=(255, 255, 255), onClick =self.Exchangewood, w = 250, h = 50),
+                        Button(200,700, txt="Steen", bgColor=(255, 255, 255), onClick =self.Exchangebrick, w = 250, h = 50),
+                        Button(200,800, txt="Ijzer", bgColor=(255, 255, 255), onClick =self.Exchangemetal, w = 250, h = 50),
+                        Button(200,900, txt="Graan", bgColor=(255, 255, 255), onClick =self.Exchangegrain, w = 250, h = 50),]
+    
+    
+        self.matsbuttons=[Button(550,600, txt="Hout", bgColor=(255, 255, 255), onClick =self.pluswood, w = 250, h = 50),
+                    Button(550,700, txt="Steen", bgColor=(255, 255, 255), onClick =self.plusstone, w = 250, h = 50),
+                    Button(550,800, txt="Ijzer", bgColor=(255, 255, 255), onClick =self.plusmetal, w = 250, h = 50),
+                    Button(550,900, txt="Graan", bgColor=(255, 255, 255), onClick =self.plusgrain, w = 250, h = 50),]
+    
+    
+    
+        self.errorbuttons=[Button(1650,350, txt="OK", bgColor=(255, 255, 255), onClick =self.NoBrickError, w = 100, h = 50),
+                    Button(500,300, txt="OK", bgColor=(255, 255, 255), onClick =self.NoGrainError, w = 100, h = 50),
+                    Button(850,750, txt="OK", bgColor=(255, 255, 255), onClick =self.NoMatserror, w = 100, h = 50),
+                    Button(1600,730, txt="OK", bgColor=(255, 255, 255), onClick =self.NoMatsppError, w = 100, h = 50),]
+    
+    def powerup1(self,self1):
+
         if self.wood>=3 and self.stone>=3 and self.metal>=3 and self.grain>=3:
             self.power1+=1
             self.wood-=3
             self.stone-=3
             self.metal-=3
-           self. grain-=3
+            self. grain-=3
         else:
             self.enoughmatspp=False
-    def powerup2(self):
-        global power2
-        global wood
-        global stone
-        global metal
-        global grain
-        global enoughmatspp
-        if self.wood>=3 and self.stone>=3 and self.metal>=3 self.and grain>=3:
+    def powerup2(self,self1):
+
+        if self.wood>=3 and self.stone>=3 and self.metal>=3 and self.grain>=3:
             self.power2+=1
             self.wood-=3
             self.stone-=3
@@ -59,14 +77,9 @@ class Markt:
             self.enoughmatspp=False
 
 
-    def powerup3(self):
-        global power3
-        global wood
-        global stone
-        global metal
-        global grain
-        global enoughmatspp
-        if wood>=3 and stone>=3 and metal>=3 and grain>=3:
+    def powerup3(self,self1):
+
+        if self.wood>=3 and self.stone>=3 and self.metal>=3 and self.grain>=3:
             self.power3+=1
             self.wood-=3
             self.stone-=3
@@ -76,13 +89,8 @@ class Markt:
             self.enoughmatspp=False
 
 
-    def powerup4(self):
-        global power4
-        global wood
-        global stone
-        global metal
-        global grain
-        global enoughmatspp
+    def powerup4(self,self1):
+
         if self.wood>=3 and self.stone>=3 and self.metal>=3 and self.grain>=3:
             self.power4+=1
             self.wood-=3
@@ -91,21 +99,15 @@ class Markt:
             self.grain-=3
         else:
             self.enoughmatspp=False
-    def chosencard(self):
-        global showcard
-        global card
-        global enoughgrain
-        global grain
-        global price
-        global effect10
-        global effect14
-        self.card=random.choice(effectcards)
+    def chosencard(self,self1):
+
+        self.card=random.choice(self.effectcards)
 
         if self.card==self.effectcards[9]:
             self.effect10=True
         if self.card==self.effectcards[13] and self.effect14==True:
-            self.card=random.choice(effectcards2)
-        if grain>=price:
+            self.card=random.choice(self.effectcards2)
+        if self.grain>=self.price:
             self.showcard=True
             self.buttons[0].enabled=False
             self.grain-=self.price
@@ -114,17 +116,14 @@ class Markt:
             self.price=3
 
 
-    def effectcard():
-        global showcard
-        global enoughgrain
+    def effectcard(self):
+
         if self.showcard==True:
             image(self.card,352,77)
 
 
-    def reset(self):
-        global showcard
-        global price
-        global effect14
+    def reset(self,self1):
+
         self.showcard=False
         self.buttons[0].enabled=True
         if self.card==self.effectcards[13]:
@@ -135,41 +134,34 @@ class Markt:
             self.price=3
 
 
-    def backpack(self):
-        global enoughbrick
-        global stone
-        global price
+    def backpack(self,self1):
+
         if self.stone>=self.price:
             self.bagcount=True
             self.buttons[2].enabled=False
-            self.stone-=price
+            self.stone-=self.price
         else:
             self.enoughbrick=False
 
 
-    def NoBrickError(self):
-        global enoughbrick
+    def NoBrickError(self,self1):
+
         self.enoughbrick=True
 
 
-    def NoGrainError(self):
-        global enoughgrain
+    def NoGrainError(self,self1):
+
         self.enoughgrain=True
 
 
-    def Effect14Used(self):
-        global effect14
+    def Effect14Used(self,self1):
+
         self.effect14=False
 
 
-    def Exchangewood(self):
-        global wood
-        global clicked
-        global resourcename
-        global enoughmats
-        global chosenmat
-        global price
-        if self.wood>self.=price:
+    def Exchangewood(self,self1):
+
+        if self.wood>=self.price:
             self.wood-=self.price
             self.clicked=True
             self.chosenmat=0
@@ -180,13 +172,8 @@ class Markt:
             self.chosenmat=5
 
 
-    def Exchangebrick(self):
-        global stone
-        global clicked
-        global resourcename
-        global enoughmats
-        global chosenmat
-        global price
+    def Exchangebrick(self,self1):
+
         if self.stone>=self.price:
             self.stone-=self.price
             self.clicked=True
@@ -198,16 +185,11 @@ class Markt:
             self.chosenmat=5
 
 
-    def Exchangemetal(self):
-        global metal
-        global clicked
-        global enoughmats
-        global resourcename
-        global chosenmat
-        global price
+    def Exchangemetal(self,self1):
+
         if self.metal>=self.price:
             self.metal-=self.price
-            self.clicked=False
+            self.clicked=True
             self.chosenmat=2
         else:
             self.resourcename='Ijzer'
@@ -216,13 +198,8 @@ class Markt:
             self.chosenmat=5
 
 
-    def Exchangegrain(self):
-        global grain
-        global clicked
-        global enoughmats
-        global resourcename
-        global chosenmat
-        global price
+    def Exchangegrain(self,self1):
+
         if self.grain>=self.price:
             self.grain-=self.price
             self.clicked=True
@@ -233,89 +210,43 @@ class Markt:
             self.enoughmats=False
             self.clicked=True
             self.chosenmat=5
-    def pluswood(self):
-        global wood
-        global stone
-        global metal
-        global grain
-        global clicked
+    def pluswood(self,self1):
+
         if (self.wood+self.metal+self.stone+self.grain)<(self.capacity):
             self.wood+=1
             self.clicked=False
 
-    def plusstone(self):
-        global wood
-        global stone
-        global metal
-        global grain
-        global clicked
+    def plusstone(self,self1):
+
         if (self.wood+self.metal+self.stone+self.grain)<(self.capacity):
-            stone+=1
-            clicked=False
+            self.stone+=1
+            self.clicked=False
     
-    def plusmetal(self):
-        global wood
-        global stone
-        global metal
-        global grain
-        global clicked
-        if (self.wood+self.metal+self.stone+self.grain)<(self.capacity):
-            metal+=1
-            clicked=False
+    def plusmetal(self,self1):
 
-    def plusgrain(self):
-        global wood
-        global stone
-        global metal
-        global grain
-        global clicked
         if (self.wood+self.metal+self.stone+self.grain)<(self.capacity):
-            grain+=1
-            clicked=False
+            self.metal+=1
+            self.clicked=False
 
-    def NoMatserror(self):
-        global clicked
-        global enoughmats
+    def plusgrain(self,self1):
+
+        if (self.wood+self.metal+self.stone+self.grain)<(self.capacity):
+            self.grain+=1
+            self.clicked=False
+
+    def NoMatserror(self,self1):
+
         self.clicked=False
         self.enoughmats=True
-    def NoMatsppError(self):
+    def NoMatsppError(self,self1):
         global enoughmatspp
         self.enoughmatspp=True    
             
-    buttons=[Button(200, 150, txt="EffectKaart", bgColor=(255, 255, 255), onClick =self.chosencard, w = 250, h = 50),
-            Button(200, 250, txt="Reset", bgColor=(255, 255, 255), onClick =self.reset, w = 250, h = 50),
-            Button(1300, 250, txt="Rugzak", bgColor=(255, 255, 255), onClick =self.backpack, w = 250, h = 50),
-            Button(850, 470, txt="Gebruik", bgColor=(255, 255, 255), onClick =self.Effect14Used, w = 250, h = 50),
-            Button(1300, 550, txt="Krachtpunt P1", bgColor=(255, 255, 255), onClick =self.powerup1, w = 250, h = 50),
-            Button(1300, 650, txt="Krachtpunt P2", bgColor=(255, 255, 255), onClick =self.powerup2, w = 250, h = 50),
-            Button(1300, 750, txt="Krachtpunt P3", bgColor=(255, 255, 255), onClick =self.powerup3, w = 250, h = 50),
-            Button(1300, 850, txt="Krachtpunt P4", bgColor=(255, 255, 255), onClick =self.powerup4, w = 250, h = 50),]
-
-
-    resourcesbuttons=[Button(200,600, txt="Hout", bgColor=(255, 255, 255), onClick =self.Exchangewood, w = 250, h = 50),
-                    Button(200,700, txt="Steen", bgColor=(255, 255, 255), onClick =self.Exchangebrick, w = 250, h = 50),
-                    Button(200,800, txt="Ijzer", bgColor=(255, 255, 255), onClick =self.Exchangemetal, w = 250, h = 50),
-                    Button(200,900, txt="Graan", bgColor=(255, 255, 255), onClick =self.Exchangegrain, w = 250, h = 50),]
-
-
-    matsbuttons=[Button(550,600, txt="Hout", bgColor=(255, 255, 255), onClick =self.pluswood, w = 250, h = 50),
-                Button(550,700, txt="Steen", bgColor=(255, 255, 255), onClick =self.plusstone, w = 250, h = 50),
-                Button(550,800, txt="Ijzer", bgColor=(255, 255, 255), onClick =self.plusmetal, w = 250, h = 50),
-                Button(550,900, txt="Graan", bgColor=(255, 255, 255), onClick =self.plusgrain, w = 250, h = 50),]
-
-
-
-    errorbuttons=[Button(1650,350, txt="OK", bgColor=(255, 255, 255), onClick =self.NoBrickError, w = 100, h = 50),
-                Button(500,300, txt="OK", bgColor=(255, 255, 255), onClick =self.NoGrainError, w = 100, h = 50),
-                Button(850,750, txt="OK", bgColor=(255, 255, 255), onClick =self.NoMatserror, w = 100, h = 50),
-                Button(1600,730, txt="OK", bgColor=(255, 255, 255), onClick =self.NoMatsppError, w = 100, h = 50),]
 
 
     def setup(self):
-        global backgr
-        global effectcards
-        global effectcards2
-        size(1920,1000)
+
+        # size(1920,1000)
         self.effectcards=[]
         self.effectcards2=[]
         self.backgr=loadImage('TITLESCREEN2.png')
@@ -331,9 +262,8 @@ class Markt:
         
 
     def draw(self):
-        global clicked
-        global effect14
-        image(backgr,0,0)
+
+        image(self.backgr,0,0)
         for self.x in range(3):
             self.buttons[self.x].draw()
             self.buttons[self.x].update()
@@ -356,7 +286,7 @@ class Markt:
             self.errorbuttons[1].update()
         if self.effect14:
             rect(700,77,304,341,5)
-            image(effectcards[13],702,79)
+            image(self.effectcards[13],702,79)
             self.buttons[3].draw()
             self.buttons[3].update()
         if self.buttons[0].enabled==True:
@@ -366,18 +296,18 @@ class Markt:
         fill(0)
         text('Wissel : ',200,500)
         text('Voor : ',550,500)
-        for self.i in range(4):
-            self.resourcesbuttons[self.i].draw()
-            self.resourcesbuttons[self.i].update()
-            self.matsbuttons[self.i].draw()
-            self.matsbuttons[self.i].update()
+        for i in range(4):
+            self.resourcesbuttons[i].draw()
+            self.resourcesbuttons[i].update()
+            self.matsbuttons[i].draw()
+            self.matsbuttons[i].update()
             if self.clicked:
-                self.resourcesbuttons[self.i].enabled=False
-                if self.enoughmats and self.i!=self.chosenmat:
-                    self.matsbuttons[self.i].enabled=True
+                self.resourcesbuttons[i].enabled=False
+                if self.enoughmats and i!=self.chosenmat:
+                    self.matsbuttons[i].enabled=True
             else:
-                self.resourcesbuttons[self.i].enabled=True
-                self.matsbuttons[self.i].enabled=False         
+                self.resourcesbuttons[i].enabled=True
+                self.matsbuttons[i].enabled=False         
             
         if not self.enoughmats:
             fill(0)
@@ -400,4 +330,4 @@ class Markt:
                 self.buttons[self.i].enabled=False
         else:
             for self.i in range(4,8):
-                buttons[self.i].enabled=True
+                self.buttons[self.i].enabled=True
